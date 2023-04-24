60f35ef0e2b2701c859d6786b96e6268e0ed7233
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-24 13:57:33 -0400

    feat: extension hospital show page

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_with_patient_counts
+    doctors
+    .joins(:patients)
+    .select('doctors.*, COUNT(patients.id) AS patient_count')    
+    .group('doctors.id')
+    .order('patient_count DESC')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :doctors, only: [:show]
   resources :appointments, only: [:destroy]
   resources :patients, only: [:index]
+  resources :hospitals, only: [:show]
 end
